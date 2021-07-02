a4360c2dc75d30e7756af35a8fac49acc1ffe0db
jorsuap <jorsuap@gmail.com>
2021-07-02 11:02:04 -0500

    master

---
+++ js/addclass.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function myFunction() {
+    var element = document.getElementById("prueba");
+    element.classList.toggle("prueba1");
+    var flecha = document.getElementById("share");
+    flecha.classList.toggle("prueba2");
+}
